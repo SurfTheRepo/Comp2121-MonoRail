9f49a63a9db6098303b8bbe1145ce7e3b2b0f4bd
z5116011-Otto <30608520+z5116011-Otto@users.noreply.github.com>
2017-10-23 16:26:22 +1100

    cuck

--- ottos-bits.asm
+++ ottos-bits.asm
@@ -98,19 +98,19 @@
 
 ;============LCD Output Macros===========
 .macro do_lcd_command
-	ldi r16, @0
+	ldi r22, @0
 	rcall lcd_command
 	rcall lcd_wait
 .endmacro
 
 .macro do_lcd_data
-	mov r16, @0
+	mov r22, @0
 	rcall lcd_data
 	rcall lcd_wait
 .endmacro
 
 .macro do_lcd_char
-	ldi r16, @0
+	ldi r22, @0
 	rcall lcd_data
 	rcall lcd_wait
 .endmacro
@@ -211,7 +211,17 @@ DEFAULT:
 
 
 RESET:
+	ldi r16, low(RAMEND)
+	out SPL, r16
+	ldi r16, high(RAMEND)
+	out SPH, r16
 
+	ser temp					;Set temp to all 1's
+	out DDRF, temp				;Port F = output
+	out DDRA, temp				;Port A = output
+	clr temp
+	out PORTF, temp
+	out PORTA, temp
 ;=============LCD SetUP=====================
 	do_lcd_command LCD_FUNC_SET		;2x5x7
 	rcall sleep_5ms
@@ -254,11 +264,11 @@ STRING_KEYPAD:
 	rowloop_string:      
 		mov temp2, temp
 		and temp2, mask ; check masked bit
-		brne skipconv ; if the result is non-zero, we need to look again
-		rcall convert ; if bit is clear, convert the bitcode
+		brne skipconv_string ; if the result is non-zero, we need to look again
+		rcall convert_string ; if bit is clear, convert the bitcode
 		jmp STRING_KEYPAD ; and start again
 
-	skipconv:
+	skipconv_string:
 		inc row ; else move to the next row
 		lsl mask ; shift the mask to the next bit
 		jmp rowloop_string
@@ -275,34 +285,34 @@ STRING_KEYPAD:
 		jmp colloop_string ; and check the next column convert function converts the row and column given to a binary number and also outputs the value to PORTC. 
 		; Inputs come from registers row and col and output is in temp.
 
-	convert:
+	convert_string:
 		cpi col, 3 ; if column is 3 we have a letter
-		breq letters
+		breq letters_string
 
 		cpi row, 3 ; if row is 3 we have a symbol or 0
-		breq symbols
+		breq symbols_string
 
 		cpi row, 0
-		breq row1
+		breq row1_string
 
 		cpi row, 1
-		breq row2
+		breq row2_string
 
 		cpi row, 2
-		breq row3
+		breq row3_string
 
-	row1:
-		ldi temp, '1'
+	row1_string:
+		ldi temp, 1
 		add temp, col ; add the column address
 		jmp convert_end_string
 
-	row2:
-		ldi temp, '4'
+	row2_string:
+		ldi temp, 4
 		add temp, col ; add the column address
 		jmp convert_end_string
 
-	row3:
-		ldi temp, '7'
+	row3_string:
+		ldi temp, 7
 		add temp, col ; add the column address
 		jmp convert_end_string
 
@@ -312,8 +322,8 @@ STRING_KEYPAD:
 	; row*3 + col + 1.
 		jmp convert_end_string
 
-	letters:
-		;ldi temp, 'A'
+	letters_string:
+		ldi temp, 'A'
 		add temp, row ; increment from 0xA by the row value
 		cpi temp, 'A'
 		breq printZ
@@ -321,11 +331,11 @@ STRING_KEYPAD:
 		printZ:
 			ldi r22, 'Z'
 			jmp printVal_string
-	symbols:
+	symbols_string:
 		cpi col, 0 ; check if we have a star
 		breq star
 
-	star:
+	star:	
 		; SAVE THE STRING
 		jmp endString
 
@@ -333,13 +343,88 @@ STRING_KEYPAD:
 		ldi temp, '0' ; set to zero
 
 	convert_end_string:
+		rcall sleep_100ms
+		rcall sleep_100ms
 		mov r22, temp
 		cpi InputCountFlag, 0
-		brne convertTwoOne
+		brne convertTwoOne_jmp
+		
         ldi InputCountFlag, 1
 		mov firstChar, r22
 		jmp endConvert_string
+
+		convertTwoOne_jmp:
+			jmp convertTwoOne
+
+		Ones:
+			cpi firstChar, 1
+			brne endConvert_string
+			ldi r22, ' ' 
+			jmp printVal_string
+		Twos:
+			cpi firstChar, 1
+			breq printA
+			cpi firstChar, 2
+			breq printB
+			cpi firstChar, 3
+			breq printC
+			jmp endConvert_string
+			printA:
+				ldi r22, 'A'
+				jmp printVal_string
+			printB:
+				ldi r22, 'B'
+				jmp printVal_string
+			printC:
+				ldi r22, 'C'
+				jmp printVal_string
+
+		Threes:
+			cpi firstChar, 1
+			breq printD
+			cpi firstChar, 2
+			breq printE
+			cpi firstChar, 3
+			breq printF
+			jmp endConvert_string
+			printD:
+				ldi r22, 'D'
+				jmp printVal_string
+			printE:
+				ldi r22, 'E'
+				jmp printVal_string	
+			printF:
+				ldi r22, 'F'
+				jmp printVal_string
+
+		Fours:
+			cpi firstChar, 1
+			breq printG
+			cpi firstChar, 2
+			breq printH
+			cpi firstChar, 3
+			breq printI
+			jmp endConvert_string
+			printG:
+				ldi r22, 'G'
+				jmp printVal_string
+			printH:
+				ldi r22, 'H'
+				jmp printVal_string
+			printI:
+				ldi r22, 'I'
+				jmp printVal_string
+
+	
+			endConvert_string:
+			rcall sleep_25ms
+			rcall sleep_25ms
+			rcall sleep_25ms
+			rcall sleep_100ms
+			ret ; return to caller
+
 		convertTwoOne:
+		
 			ldi InputCountFlag, 0
 			
 			cpi r22, 1
@@ -356,7 +441,27 @@ STRING_KEYPAD:
 
 			cpi r22, 5
 			breq Fives
+			jmp nextChunk
 
+			Nines:
+				cpi firstChar, 1
+				breq printW
+				cpi firstChar, 2
+				breq printX
+				cpi firstChar, 3
+				breq printY
+				jmp endConvert_string
+				printW:
+					ldi r22, 'W'
+					jmp printVal_string
+				printX:
+					ldi r22, 'X'
+					jmp printVal_string
+				printY:
+					ldi r22, 'Y'
+					jmp printVal_string
+
+			nextChunk:
 			cpi r22, 6
 			breq Sixes
 
@@ -368,66 +473,8 @@ STRING_KEYPAD:
 			
 			cpi r22, 9
 			breq Nines
-
-			Ones:
-				cpi firstChar, 1
-				brne endConvert_string
-				ldi r22, ' ' 
-				jmp printVal_string
-			Twos:
-				cpi firstChar, 1
-				breq printA
-				cpi firstChar, 2
-				breq printB
-				cpi firstChar, 3
-				breq printC
-				jmp endConvert_string
-				printA:
-					ldi r22, 'A'
-					jmp printVal_string
-
-				printB:
-					ldi r22, 'B'
-					jmp printVal_string
-				printC:
-					ldi r22, 'C'
-					jmp printVal_string
-
-			Threes:
-				cpi firstChar, 1
-				breq printD
-				cpi firstChar, 2
-				breq printE
-				cpi firstChar, 3
-				breq printF
-				jmp endConvert_string
-				printD:
-					ldi r22, 'D'
-					jmp printVal_string
-				printE:
-					ldi r22, 'E'
-					jmp printVal_string	
-				printF:
-					ldi r22, 'F'
-					jmp printVal_string
-
-			Fours:
-				cpi firstChar, 1
-				breq printG
-				cpi firstChar, 2
-				breq printH
-				cpi firstChar, 3
-				breq printI
-				jmp endConvert_string
-				printG:
-					ldi r22, 'G'
-					jmp printVal_string
-				printH:
-					ldi r22, 'H'
-					jmp printVal_string
-				printI:
-					ldi r22, 'I'
-					jmp printVal_string
+			
+		
 
 			Fives:
 				cpi firstChar, 1
@@ -502,34 +549,13 @@ STRING_KEYPAD:
 					ldi r22, 'V'
 					jmp printVal_string
 
-			Nines:
-				cpi firstChar, 1
-				breq printW
-				cpi firstChar, 2
-				breq printX
-				cpi firstChar, 3
-				breq printY
-				jmp endConvert_string
-				printW:
-					ldi r22, 'W'
-					jmp printVal_string
-				printX:
-					ldi r22, 'X'
-					jmp printVal_string
-				printY:
-					ldi r22, 'Y'
-					jmp printVal_string
+			
 
 		printVal_string:
 			rcall lcd_data
 			rcall lcd_wait
 			rjmp endConvert_string
-		endConvert_string:
-		rcall sleep_25ms
-		rcall sleep_25ms
-		rcall sleep_25ms
-		rcall sleep_100ms
-		ret ; return to caller
+	
 endString:
 	ldi r22, '='
 	rcall lcd_data
@@ -537,3 +563,88 @@ endString:
 	rjmp endConvert_string
 	; called when '*' is pressed
 	;trigger to set end of string, and ask for new input or run emulator
+
+
+	;==========LCD Display Code Below (Note r16)==========
+; Delay Constants
+.equ F_CPU = 16000000
+.equ DELAY_1MS = F_CPU / 4 / 1000 - 4 				; 4 cycles per iteration - setup/call-return overhead
+
+.equ LCD_RS = 7
+.equ LCD_E = 6
+.equ LCD_RW = 5
+.equ LCD_BE = 4
+
+lcd_command:
+	out PORTF, r22
+	rcall sleep_1ms
+	lcd_set LCD_E
+	rcall sleep_1ms
+	lcd_clr LCD_E
+	rcall sleep_1ms
+	ret
+
+lcd_data:
+	out PORTF, r22
+	lcd_set LCD_RS
+	rcall sleep_1ms
+	lcd_set LCD_E
+	rcall sleep_1ms
+	lcd_clr LCD_E
+	rcall sleep_1ms
+	lcd_clr LCD_RS
+	ret
+
+lcd_wait:
+	push r22
+	clr r22
+	out DDRF, r22
+	out PORTF, r22
+	lcd_set LCD_RW
+lcd_wait_loop:
+	rcall sleep_1ms
+	lcd_set LCD_E
+	rcall sleep_1ms
+	in r22, PINF
+	lcd_clr LCD_E
+	sbrc r22, 7
+	rjmp lcd_wait_loop
+	lcd_clr LCD_RW
+	ser r22
+	out DDRF, r22
+	pop r22
+	ret
+
+sleep_1ms:
+	push r24
+	push r25
+	ldi r25, high(DELAY_1MS)
+	ldi r24, low(DELAY_1MS)
+delayloop_1ms:
+	sbiw r25:r24, 1
+	brne delayloop_1ms
+	pop r25
+	pop r24
+	ret
+
+sleep_5ms:
+	rcall sleep_1ms
+	rcall sleep_1ms
+	rcall sleep_1ms
+	rcall sleep_1ms
+	rcall sleep_1ms
+	ret
+
+sleep_25ms:
+	rcall sleep_5ms
+	rcall sleep_5ms
+	rcall sleep_5ms
+	rcall sleep_5ms
+	rcall sleep_5ms
+	ret
+sleep_100ms:
+	rcall sleep_25ms
+	rcall sleep_25ms
+	rcall sleep_25ms
+	rcall sleep_25ms
+	ret
