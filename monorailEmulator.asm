5ed57c9e81c725551f9037ff017d53071bdf86ca
z5116011-Otto <30608520+z5116011-Otto@users.noreply.github.com>
2017-10-24 15:42:25 +1100

    indents

--- monorailEmulator.asm
+++ monorailEmulator.asm
@@ -3,150 +3,152 @@
 .include "m2560def.inc"
 ;==============CONSTANTS================;
 
-.equ PORTLDIR = 0xF0
-.equ INITCOLMASK = 0xEF
-.equ INITROWMASK = 0x01
-.equ ROWMASK = 0x0F
+	.equ PORTLDIR = 0xF0
+	.equ INITCOLMASK = 0xEF
+	.equ INITROWMASK = 0x01
+	.equ ROWMASK = 0x0F
 
-.equ F_CPU = 16000000
-.equ DELAY_1MS = F_CPU / 4 / 1000 - 4
+	.equ F_CPU = 16000000
+	.equ DELAY_1MS = F_CPU / 4 / 1000 - 4
+
+	.equ LCD_RS = 7
+	.equ LCD_E = 6
+	.equ LCD_RW = 5
+	.equ LCD_BE = 4
+	.set LCD_HOME_LINE = 0b00000001
 
 ;==========LCD Commands==========
-.set LCD_DISP_ON = 0b00001110
-.set LCD_DISP_OFF = 0b00001000
-.set LCD_DISP_CLR = 0b00000001
+	.set LCD_DISP_ON = 0b00001110
+	.set LCD_DISP_OFF = 0b00001000
+	.set LCD_DISP_CLR = 0b00000001
 
-.set LCD_FUNC_SET = 0b00111000 						; 2 lines, 5 by 7 characters
-.set LCD_ENTR_SET = 0b00000110 						; increment, no display shift
-.set LCD_HOME_LINE = 0b10000000 					; goes to 1st line (address 0)
-.set LCD_SEC_LINE = 0b10101000 						; goes to 2nd line (address 40)
+	.set LCD_FUNC_SET = 0b00111000 						; 2 lines, 5 by 7 characters
+	.set LCD_ENTR_SET = 0b00000110 						; increment, no display shift
+	.set LCD_HOME_LINE = 0b10000000 					; goes to 1st line (address 0)
+	.set LCD_SEC_LINE = 0b10101000 						; goes to 2nd line (address 40)
 ;=================================
 
 ;================DEFINITIONS=================
 
-.def temp = r16
-.def temp2 = r17
-.def mask = r18
-.def col = r19
-.def row = r20
+	.def temp = r16
+	.def temp2 = r17
+	.def mask = r18
+	.def col = r19
+	.def row = r20
 
-.def InputCountFlag = r24
-.def firstChar = r21
-.def stringLength = r23
+	.def InputCountFlag = r24
+	.def firstChar = r21
+	.def stringLength = r23
 
 ;===============MACROS======================
-.macro conflictPush
-	push r0
-	push r1
-	push r2
-	push r3
-	push r5
-	push r6
-	push r7
-	push r8
-	push r9
-	push r10
-	push r11
-	push r12
-	push r13
-	push r14
-	push r15
-	push r16
-	push r17
-	push r18
-	push r19
-	push r20
-	push r21
-	push r22
-	push r23
-	push r24
-	push r25
-	push r26
-	push r27
-	push r28
-	push r29
-	push r30
-	push r31
-	in r16, SREG
-	push r16
-.endmacro
+	.macro conflictPush
+		push r0
+		push r1
+		push r2
+		push r3
+		push r5
+		push r6
+		push r7
+		push r8
+		push r9
+		push r10
+		push r11
+		push r12
+		push r13
+		push r14
+		push r15
+		push r16
+		push r17
+		push r18
+		push r19
+		push r20
+		push r21
+		push r22
+		push r23
+		push r24
+		push r25
+		push r26
+		push r27
+		push r28
+		push r29
+		push r30
+		push r31
+		in r16, SREG
+		push r16
+	.endmacro
 
 
-.macro conflictPop
-	pop r16
-	out SREG, r16
-	pop r31
-	pop r30
-	pop r29
-	pop r28
-	pop r27
-	pop r26
-	pop r25
-	pop r24
-	pop r23
-	pop r22
-	pop r21
-	pop r20
-	pop r19
-	pop r18
-	pop r17
-	pop r16
-	pop r15
-	pop r14
-	pop r13
-	pop r12
-	pop r11
-	pop r10
-	pop r9
-	pop r8
-	pop r7
-	pop r6
-	pop r5
-	pop r4
-	pop r3
-	pop r2
-	pop r1
-	pop r0
-.endmacro
+	.macro conflictPop
+		pop r16
+		out SREG, r16
+		pop r31
+		pop r30
+		pop r29
+		pop r28
+		pop r27
+		pop r26
+		pop r25
+		pop r24
+		pop r23
+		pop r22
+		pop r21
+		pop r20
+		pop r19
+		pop r18
+		pop r17
+		pop r16
+		pop r15
+		pop r14
+		pop r13
+		pop r12
+		pop r11
+		pop r10
+		pop r9
+		pop r8
+		pop r7
+		pop r6
+		pop r5
+		pop r4
+		pop r3
+		pop r2
+		pop r1
+		pop r0
+	.endmacro
 
-;; LCD STUFF
-.equ LCD_RS = 7
-.equ LCD_E = 6
-.equ LCD_RW = 5
-.equ LCD_BE = 4
-.set LCD_HOME_LINE = 0b00000001
 
-.macro do_lcd_command
-	ldi r21, @0
-	rcall lcd_command
-	rcall lcd_wait
-.endmacro
+	
 
-.macro funky_do_lcd_command
-	mov r21, @0
-	rcall lcd_command
-	rcall lcd_wait
-.endmacro
+	.macro do_lcd_command
+		ldi r21, @0
+		rcall lcd_command
+		rcall lcd_wait
+	.endmacro
 
-.macro do_lcd_data
-	mov r22, @0
-	rcall lcd_data
-	rcall lcd_wait
-.endmacro
+	.macro funky_do_lcd_command
+		mov r21, @0
+		rcall lcd_command
+		rcall lcd_wait
+	.endmacro
 
-.macro do_lcd_char
-	ldi r22, @0
-	rcall lcd_data
-	rcall lcd_wait
-.endmacro
+	.macro do_lcd_data
+		mov r22, @0
+		rcall lcd_data
+		rcall lcd_wait
+	.endmacro
 
-.macro lcd_set
-	sbi PORTA, @0
-.endmacro
+	.macro do_lcd_char
+		ldi r22, @0
+		rcall lcd_data
+		rcall lcd_wait
+	.endmacro
 
-.macro lcd_clr
-	cbi PORTA, @0
-.endmacro
+	.macro lcd_set
+		sbi PORTA, @0
+	.endmacro
+
+	.macro lcd_clr
+		cbi PORTA, @0
+	.endmacro
 
 ;==========================END MACROS=======================
 
@@ -224,9 +226,10 @@
 	jmp DEFAULT 					; Timer/Counter3 Overflow
 .org 0x0072
 ;================STRING CONSTANTS===================;
-Print_Enter_Stations: .db "Enter the numberof stations: " ;16-12
-Print_Give_Stn_Names: .db "Giv Stn Name: "
-Print_Not_Int: .db "Input 1-9"	;9
+	Print_Enter_Stations: .db "Enter the numberof stations: " ;16-12
+	Print_Give_Stn_Names: .db "Giv Stn Name: "
+	Print_Not_Int: .db "Input 1-9"	;9
+
 DEFAULT:
 	reti							; used for interrupts that are not handled
 
@@ -978,62 +981,62 @@ INT_KEYPAD:
 	
 
 ;=================================LCD STUFF===========================;
-//LCD COMMANDS
-lcd_command:
-	out PORTF, r21
-	nop
-	lcd_set LCD_E
-	nop
-	nop
-	nop
-	lcd_clr LCD_E
-	nop
-	nop
-	nop
-	ret
-
-lcd_data:
-	out PORTF, r22
-	lcd_set LCD_RS
-	nop
-	nop
-	nop
-	lcd_set LCD_E
-	nop
-	nop
-	nop
-	lcd_clr LCD_E
-	nop
-	nop
-	nop
-	lcd_clr LCD_RS
-	ret
-
-lcd_wait:
-	push r21
-	clr r21
-	out DDRF, r21
-	out PORTF, r21
-	lcd_set LCD_RW
-
-	lcd_wait_loop:
+LCD:
+	lcd_command:
+		out PORTF, r21
 		nop
 		lcd_set LCD_E
 		nop
 		nop
 		nop
-		in r21, PINF
 		lcd_clr LCD_E
-		sbrc r21, 7
-		rjmp lcd_wait_loop
-	
-	lcd_clr LCD_RW
-	ser r21
-	out DDRF, r21
-	pop r21
-	ret
+		nop
+		nop
+		nop
+		ret
 
-;sleepstuff
+	lcd_data:
+		out PORTF, r22
+		lcd_set LCD_RS
+		nop
+		nop
+		nop
+		lcd_set LCD_E
+		nop
+		nop
+		nop
+		lcd_clr LCD_E
+		nop
+		nop
+		nop
+		lcd_clr LCD_RS
+		ret
+
+	lcd_wait:
+		push r21
+		clr r21
+		out DDRF, r21
+		out PORTF, r21
+		lcd_set LCD_RW
+
+		lcd_wait_loop:
+			nop
+			lcd_set LCD_E
+			nop
+			nop
+			nop
+			in r21, PINF
+			lcd_clr LCD_E
+			sbrc r21, 7
+			rjmp lcd_wait_loop
+		
+		lcd_clr LCD_RW
+		ser r21
+		out DDRF, r21
+		pop r21
+		ret
+
+sleepstuff:
 	sleep_1ms:
 		push r24
 		push r25
